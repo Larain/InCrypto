cf851431f2eebe40b2c15c4e8451026169df7b01
Олейник Юрий Русланович <gostwel@gmail.com>
2016-12-20 17:31:18 +0200

    Refactoring for model

--- CryptoProvider/Abstract/ICryptoProvider.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace icModel.Abstract {
-    public interface ICryptoProvider {
-        ICryptoKey CryptoKey { get; set; }
-        CryptoMethod CryptoMethod { get; set; }
-
-        string[] Encrypt(string[] message);
-        string[] Decrypt(string[] message);
-    }
-}
